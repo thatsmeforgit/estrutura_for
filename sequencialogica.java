37fde72ef61d4ef8f294eddda4ba6b5d254065e7
thatsmeforgit <igorifpr2015@gmail.com>
2023-06-16 14:50:08 -0300

    commit

--- sequencialogica.java
+++ sequencialogica.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
-class sequencialogica {
-    public static void main(String[] args){
-        int n=0, i=0;
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class sequenciaLogica {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        //in
         Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
+        int n = sc.nextInt();
         sc.close();
-
-        for(i=1; i<=n;i = i+1){
-            System.out.printf("%d %d %d\n",i, i*i, i*i*i);
-            System.out.printf("%d %d %d\n",i, i*i+1, i*i*i+1);
+        
+        //process & out
+        for(int i=1;i<=n;i++){
+            System.out.printf("%d %d %d\n", i, i*i, i*i*i);
+            System.out.printf("%d %d %d\n", i, i*i+1, i*i*i+1);
         }
-
-    }    
+    }
+ 
 }
